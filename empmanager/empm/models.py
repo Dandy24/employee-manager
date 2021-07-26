f72e0ad34bf2d71db2c0c2114c0cd4c58a5faa68
Dan Godula <dan.godula@seznam.cz>
2021-07-26 21:56:44 +0200

    Extended Employee table

--- empmanager/empm/models.py
+++ empmanager/empm/models.py
@@ -9,3 +9,13 @@ class Employee(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email = models.EmailField(blank=True, unique=True)
+    health_limitations = models.TextField(blank=True)
+    active = models.BooleanField(default=True)
+    working_category = models.CharField(max_length=1)
+    med_exam_date = models.DateField(blank=True, null=True)
+    job_assign_date = models.DateField(blank=True, null=True)
+    company = models.ForeignKey(
+        'Company',
+        on_delete=models.CASCADE,
+        null=True
+    )
