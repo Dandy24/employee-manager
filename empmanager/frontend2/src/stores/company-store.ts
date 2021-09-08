97ef37e937a57d655b9ab0968232043766bdfe3b
Dan Godula <dan.godula@seznam.cz>
2021-09-08 19:22:55 +0200

    Company API calls return types

--- empmanager/frontend2/src/stores/company-store.ts
+++ empmanager/frontend2/src/stores/company-store.ts
@@ -2,6 +2,7 @@ import { RootStore } from './root-store';
 import { action, makeObservable, observable, runInAction } from 'mobx';
 import { createCompany, deleteCompany, getCompanyList, updateCompany } from '../api/apiCalls';
 import { message } from 'antd';
+import { CompanyEntity } from '../models/entities/company-entity';
 
 export class CompanyStore {
     companies: any[] = []; //TODO type  Employee[]
@@ -24,6 +25,7 @@ export class CompanyStore {
             openToEdit: action,
             addCompany: action,
             editCompany: action,
+            updateCompany: action,
             deleteCompany: action,
         });
     }
@@ -65,7 +67,7 @@ export class CompanyStore {
 
     async editCompany(company: any): Promise<void> {
         if (this.company.id) {
-            await updateCompany(company, this.company.id)
+            const updatedCompany = await updateCompany(company, this.company.id)
                 .then(() => {
                     message.warning('Údaje o společnosti byly upraveny');
                 })
@@ -73,8 +75,14 @@ export class CompanyStore {
                     message.error('Údaje o společnosti se nepodařilo upravit.');
                     console.log(error);
                 }); //TODO dat to ID pryc nejak
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            if (updatedCompany) {
+                this.updateCompany(updatedCompany);
+            }
+            await this.fetchAllCompanies();
         }
-        await this.fetchAllCompanies();
+
         runInAction(() => {
             this.closeModal();
         });
@@ -97,7 +105,9 @@ export class CompanyStore {
 
     async deleteCompany(company: any): Promise<void> {
         //TODO type
-        await deleteCompany(company.id);
+        await deleteCompany(company.id).catch(() => {
+            message.error('Firmu se nepodařilo smazat.');
+        });
         runInAction(() => {
             this.loadingCompanies = true;
             message.success('Firma byla úspěšně smazána.');
@@ -105,6 +115,15 @@ export class CompanyStore {
         await this.fetchAllCompanies();
         runInAction(() => {
             this.loadingCompanies = false;
+        });
+    }
+
+    updateCompany(updatedCompany: any): void {
+        runInAction(() => {
+            const updatedCompanyIndex = this.companies.findIndex((company) => company.id === updatedCompany.id);
+            if (updatedCompanyIndex !== -1) {
+                this.companies[updatedCompanyIndex] = updatedCompanyIndex;
+            }
         });
     }
 }
