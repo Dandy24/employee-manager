eaaf98cfe34bc2165f0018101c46846ec898dc2e
Dan Godula <67206290+Dandy24@users.noreply.github.com>
2021-10-10 13:41:15 +0200

    chore: add ESLint plugins for testing (#3)

--- empmanager/frontend2/.eslintrc.js
+++ empmanager/frontend2/.eslintrc.js
@@ -4,8 +4,10 @@ module.exports = {
         'plugin:prettier/recommended', // Uses the recommended rules from @eslint-plugin-react
         'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
         'plugin:@typescript-eslint/recommended', // Uses the recommended rules from @typescript-eslint/eslint-plugin
+        'plugin:testing-library/react', // For React Testing Library
+        'plugin:cypress/recommended', // For Cypress (recommended config)
     ],
-    plugins: ['better-styled-components'],
+    plugins: ['better-styled-components', 'testing-library', 'cypress'],
     parserOptions: {
         ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
         sourceType: 'module', // Allows for the use of imports
@@ -18,6 +20,11 @@ module.exports = {
         // '@typescript-eslint/no-empty-interface': 'off',
         // '@typescript-eslint/no-comma-dangle': 'off',
         // '@typescript-eslint/explicit-function-return-type': 'off',
+        'testing-library/await-async-query': 'error',
+        'testing-library/no-await-sync-query': 'error',
+        'testing-library/no-debug': 'warn',
+        'testing-library/no-dom-import': 'off',
+        /////////////////
         'react/display-name': 'off',
         'better-styled-components/sort-declarations-alphabetically': 2,
         'prettier/prettier': [
@@ -43,6 +50,11 @@ module.exports = {
             rules: {
                 'react/prop-types': 'off',
             },
+        },
+        {
+            // 3) Testing library ESLint to run only in files located in __test__ dir
+            files: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+            extends: ['plugin:testing-library/react'],
         },
     ],
 };
