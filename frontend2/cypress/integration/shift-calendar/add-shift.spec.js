fefb42781a21196de6bf403babdeb9552ec7a3a7
Dan Godula <dan.godula@seznam.cz>
2022-01-03 13:34:25 +0100

    refactor: fixed data in test

--- frontend2/cypress/integration/shift-calendar/add-shift.spec.js
+++ frontend2/cypress/integration/shift-calendar/add-shift.spec.js
@@ -8,13 +8,13 @@ describe('proccess of creating shift through company calendar', () => {
     beforeEach(() => {
         deleteAllShifts();
         cy.waitUntil(() =>
-            createShift({ date: testDate, time: ShiftTypeEnum.Rano, companyID: 59, employeeIDs: [37, 43] }),
+            createShift({ date: testDate, time: ShiftTypeEnum.Rano, companyID: 1, employeeIDs: [37, 43] }),
         );
         cy.waitUntil(() =>
-            createShift({ date: testDate, time: ShiftTypeEnum.Vecer, companyID: 59, employeeIDs: [46, 19] }),
+            createShift({ date: testDate, time: ShiftTypeEnum.Vecer, companyID: 1, employeeIDs: [46, 19] }),
         );
         cy.visit('/company-list');
-        cy.get('[data-testid=company-calendar-button-59]').click();
+        cy.get('[data-testid=company-calendar-button-1]').click();
     });
 
     it('Tests if the image snapshot of company calendar matches ', () => {
@@ -22,24 +22,24 @@ describe('proccess of creating shift through company calendar', () => {
         cy.document().toMatchImageSnapshot();
     });
 
-    // it('tests calender and header showing correctly', () => {
-    //     cy.get('main > .ant-picker-calendar-full').should('exist').and('be.visible');
-    //     cy.get('[data-testid=header-company-name]').should('have.text', 'Example Company');
-    // });
-    //
-    // it('tests calendar to have proper date set', () => {
-    //     cy.get('main > .ant-picker-calendar-full')
-    //         .find('.ant-picker-calendar-year-select')
-    //         .should('exist')
-    //         .and('be.visible')
-    //         .and('have.text', moment().format('yyyy'));
-    //     cy.get('main > .ant-picker-calendar-full')
-    //         .find('.ant-picker-calendar-month-select')
-    //         .should('exist')
-    //         .and('be.visible')
-    //         .and('have.text', moment().format('MMM'));
-    //     cy.get('[data-testid=header-company-name]').should('have.text', 'Example Company');
-    // });
+    it('tests calender and header showing correctly', () => {
+        cy.get('main > .ant-picker-calendar-full').should('exist').and('be.visible');
+        cy.get('[data-testid=header-company-name]').should('have.text', 'Test Company123');
+    });
+
+    it('tests calendar to have proper date set', () => {
+        cy.get('main > .ant-picker-calendar-full')
+            .find('.ant-picker-calendar-year-select')
+            .should('exist')
+            .and('be.visible')
+            .and('have.text', moment().format('yyyy'));
+        cy.get('main > .ant-picker-calendar-full')
+            .find('.ant-picker-calendar-month-select')
+            .should('exist')
+            .and('be.visible')
+            .and('have.text', moment().format('MMM'));
+        cy.get('[data-testid=header-company-name]').should('have.text', 'Test Company123');
+    });
 
     it('tests shifts showing up correctly in calendar', () => {
         cy.get(`[data-testid=shift-list-for-${testDate}]`).find('li').should('have.length', 2);
@@ -57,6 +57,7 @@ describe('proccess of creating shift through company calendar', () => {
         cy.get(`[title="${testDate}"]`).find('.ant-badge').should('exist');
     });
 
+    //FIXME wrap whole row in clickable link, fix getting button in [data-testid=shift-ranni], its currently only on text, not whole row including button
     it('deleting existing shift', () => {
         cy.get(`[title="${testDate}"]`).find('.ant-picker-calendar-date-content').dblclick();
 
@@ -88,24 +89,23 @@ describe('proccess of creating shift through company calendar', () => {
         cy.get('[data-testid=new-shift-Odpoledne]').click(); //should('have', 3); //TODO BE LINK
     });
 
+    //FIXME
     it('checks if data in new shift are correct after redirect', () => {
         cy.get(`[title="${testDate}"]`).find('.ant-picker-calendar-date-content').dblclick();
         cy.get('[data-testid=shift-add-button]').click();
         cy.get('[data-testid=new-shift-Odpoledne]').click();
 
-        cy.get('.ant-page-header').find('.ant-page-header-heading-left').should('contain.text', 'Example Company');
+        cy.get('.ant-page-header').find('.ant-page-header-heading-left').should('contain.text', 'Test Company123');
         cy.get('.ant-page-header')
             .find('.ant-page-header-heading-left')
             .should('contain.text', moment().subtract(1, 'days').format('MMMM Do YYYY'));
-        // cy.get('.ant-page-header')
-        //     .find('.ant-page-header-heading')
-        //     .find('ant-tag ant-tag-blue')
-        //     .should('contain.text', 'odpoledne')
-        //     .and('be.visible');
+        cy.get('.ant-page-header')
+            .get('.ant-page-header-heading-tags > .ant-tag')
+            .should('have.text', 'odpoledne')
+            .and('be.visible');
 
-        cy.get('[data-testid=employee-table-body]').find('tr').should('have.length', 5);
+        cy.get('[data-testid=employee-table-body]').find('tr').should('have.length', 3);
     });
 
-    /** TODO SAVE SHIFT WITH NO EMPLOYEES **/
-    /** TODO SHOW SOME SUCCESS STATE AFTER SAVING SHIFT  maybe https://ant.design/components/result/ ? **/
+    /** TODO SAVE SHIFT WITH NO EMPLOYEES ????????? **/
 });
